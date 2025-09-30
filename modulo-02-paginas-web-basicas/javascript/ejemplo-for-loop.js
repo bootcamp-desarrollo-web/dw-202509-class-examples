2b7162e3902ba29d390161da7b4fbc65f14f849a
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2025-09-30 17:07:30 +0200

    Add for loop example

---
+++ modulo-02-paginas-web-basicas/javascript/ejemplo-for-loop.js
@@ -0,0 +1,11 @@
+let myArr = ['Dani', 'Elinor', 'María', 'Jorge'];
+
+console.log(myArr.length)
+
+// Imprimir el último elemento
+console.log(myArr[myArr.length - 1]);
+
+// Iterar sobre todo el array e imprimir los nombres unos por uno
+for (let i = 0; i < myArr.length ; i++) {
+  console.log(myArr[i]);
+}  
