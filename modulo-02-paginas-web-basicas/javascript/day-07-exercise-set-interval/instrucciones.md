80d2a552747bb78c739e68b26877f3f945332998
Eurofirms foundation <guy.aloni@fundacioneurofirms.org>
2025-10-08 17:21:37 +0200

    Add setTimeout and setInterval example

---
+++ modulo-02-paginas-web-basicas/javascript/day-07-exercise-set-interval/instrucciones.md
@@ -0,0 +1,6 @@
+# Cambiar estilos usando setTimeout y setInterval
+
+1.Creat un HTML con un único div rectangular (200x200px), con un color de fondo (por ejemplo azul). Definir el color con una clase css
+2. Definir otra clase css que pueda dar al div otro color de fondo
+3. Usando la función setTimeout cambia el color del div 1 segundo después de que la página se carga
+4. Usando la función setInterval hacer que el color de fondo del div cambie cada 1 segundo 10 veces
---
+++ modulo-02-paginas-web-basicas/javascript/day-07-exercise-set-interval/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let counter = 0;
+    document.getElementById('mainDiv').innerText = counter;
+
+    // PARTE 1: 
+    // usar setTimeout para que el div cambie su color después de 1 segundo
+    setTimeout(() => {
+        document.getElementById('mainDiv').classList.toggle('bg1');
+        document.getElementById('mainDiv').classList.toggle('bg2');
+    }, 1000);
+
+    // PARTE 2: 
+    // usar setInterval para que el div alterne su color cada segundo 10 veces
+    const intervalId = setInterval(() => {
+        document.getElementById('mainDiv').classList.toggle('bg1');
+        document.getElementById('mainDiv').classList.toggle('bg2');
+        counter++;
+        document.getElementById('mainDiv').innerText = counter;
+
+        if (counter >= 10) {
+            clearInterval(intervalId);
+        }
+    }, 1000);
+})
